5a17609974ebf969eff0ba936892ea08896daa87
KindSpidey <44064808+KindSpidey@users.noreply.github.com>
2021-09-22 20:49:57 +0500

    everything
    ad

---
+++ Assets/Jump.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
+    }
+}
